e0f7ce5fcca48537eea6c03335577b326906cba5
Aluno Dendezeiros <aleixojosent@gmail.com>
2026-03-23 15:23:58 -0300

    atividades

--- 2-atvidade/script.js
+++ 2-atvidade/script.js
@@ -1,44 +1,48 @@
-function mediar (n1, n2) {
-    return (n1 + n2) / 2
+function gerarResultados(){
 
+    
+    function med (numero_1, numero_2) {
+        return (numero_1 + numero_2) / 2
+    }
+    
+    function som (numero_1, numero_2) {
+        return numero_1 + numero_2
+    }
+    
+    function prod (numero_1, numero_2) {
+        return numero_1 * numero_2
+    }
+    
+    function mais (numero_1, numero_2) {
+        return numero_1 > numero_2
+    }
+    
+    function menos (numero_1, numero_2) {
+        return numero_1 < numero_2
+    }
+    
+    const numeroInput_1 = document.getElementById("numeroInput_1");
+    let numero_1 = parseFloat (numeroInput_1.value);
+    
+    const numeroInput_2 = document.getElementById("numeroInput_2");
+    let numero_2 = parseFloat (numeroInput_2.value);
+    
+    const resultadoDiv = document.getElementById("resultadoDiv")
+
+    if (isNaN(numero_1) || isNaN (numero_2) || numero_1 === "" || numero_2 ==="") {
+        document.getElementById("resultadoDiv").innerHTML = "<p>Digite um número válido</p>"
+        return; 
+    }
+    
+   let gerarResultado = `
+        Soma: ${som(numero_1, numero_2)}<br>
+        Média : ${med(numero_1, numero_2)}<br>
+        Produto : ${prod(numero_1, numero_2)}<br>
+        Maior?  ${mais(numero_1, numero_2)}<br>
+        menor?  ${menos(numero_1, numero_2)}
+    `;
+
+    resultadoDiv.innerHTML = gerarResultado;
 }
-function somar (n1, n2) {
-    return n1 + n2
 
-}
-function produtor (n1, n2) {
-    return n1 * n2
-
-}
-function maior  (n1, n2) {
-    return n1 > n2
-
-}
-function menor (n1, n2) {
-    return n1 < n2
-
-}
-
-function resultadoDiv () {
-    const input1 = document.getElementById("numeroInput1");
-    const input2 = document.getElementById("numeroInput2");
-    const resultadoDiv = document.getElementById("resultadoDiv");
-}
-
-let n1 = parseFloat (input1.value);
-let n2 = parseFloat (input2.value);
-
-resultadoDiv.innerHTML = "";
-
-if (isNaN(n1) || isNaN(n2)) {
-    resultadoDiv.innerHTML = "<p>digite númeors validos</p>"
-}
-
-resultadoDiv.innerHTML +=`<p> Soma: ${somar(n1, n2)} </p>`
-resultadoDiv.innerHTML +=`<p> Soma: ${mediar(n1, n2)} </p>`
-resultadoDiv.innerHTML +=`<p> Soma: ${produtor(n1, n2)} </p>`
-resultadoDiv.innerHTML +=`<p> Soma: ${maior(n1, n2) ? "Sim" : "Não" } </p>`
-resultadoDiv.innerHTML +=`<p> Soma: ${menor(n1, n2) ? "Sim" : "Não"} </p>`
-
-const botao = document.getElementById("btnExibir");
-botao.addEventListener("click", resultadoDiv);
+document.getElementById("btnExibir").addEventListener("click", gerarResultados);
