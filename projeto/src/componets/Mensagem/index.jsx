8133c265b686f1de0a795153e4fb48db91119968
kaye <kaykysampaio13@gmail.com>
2026-03-26 16:55:55 -0300

    react

---
+++ projeto/src/componets/Mensagem/index.jsx
@@ -0,0 +1,14 @@
+import './style.css'
+
+function Mensagem() {
+    return (
+        // Comandos HTML.
+        <div className='mensagem'>
+            <h1>Compomente</h1>
+            <p>Este é um componemte com uma mensagem simples.</p>
+        </div>
+    )
+    
+}
+
+export default Mensagem 
